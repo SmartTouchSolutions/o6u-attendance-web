17ffd3c9a7b474fe841a83a6ae9ccb56c8015a57
mahmoud abass <abassmahmoud206@gmail.com>
2019-12-30 17:45:28 +0200

    teacher

--- resources/views/admin/teacher/create.blade.php
+++ resources/views/admin/teacher/create.blade.php
@@ -1,46 +1,75 @@
-{{--@extends('layouts.adminapp')--}}
-@section('title' , 'Add Teacher Assistant')
+@extends('layouts.adminapp')
+@section('content')
+    @php $noSidebar=''; @endphp
 
-<form action="{{route('TeacherAssistant.store')}}" method="post">
-    {{ csrf_field() }}
-
-    <div class="create-cont-form">
-        <label> - username :</label>
-        <div class="mb-3">
-            <input type="text" name="username" class="form-control" placeholder="username" aria-label="Username">
-        </div>
-        @if ($errors->has('username'))
-            <p class="alert alert-danger">{{ $errors->first('username') }}</p>
-        @endif
-
-        <label> <i class="fas fa-file-alt"></i> email :</label>
-        <div class="mb-3">
-            <input type="email"  name="email" class="form-control" placeholder="email" aria-label="email">
-        </div>
-        @if ($errors->has('email'))
-            <p class="alert alert-danger">{{ $errors->first('email') }}</p>
-        @endif
-
-        <label> <i class="fas fa-eye-slash"></i> Password :</label>
-        <div class="mb-3">
-            <input name="password" type="password" class="form-control" placeholder="Input Passowrd" aria-label="Username">
-        </div>
-        @if ($errors->has('password'))
-            <p class="alert alert-danger">{{ $errors->first('password') }}</p>
-        @endif
-
-
-        subjects<br>
-        @foreach($allSubjects as $subject)
-            <input id="{{$subject->name}}" type="checkbox" name="subjects[]" value="{{$subject->id}}"><label for="{{$subject->name}}">{{$subject->name}}</label><br>
-        @endforeach
-        @if ($errors->has('subjects'))
-            <p class="alert alert-danger">{{ $errors->first('subjects') }}</p>
-        @endif
-
-    </div>
-    <div class="create-cont-footer">
-        <div class="row">
-            <div class="col-lg-1 order-last  col-8">
-                <button  type="submit" class="btn btn-reset">Submit</button>
+    <div class="title3">
+        <div class="row" style="margin:0;">
+            <div class="col-md-5 my-2">
+                <button type="button" id="sidebarCollapse" class="btn stf">
+                    <i class="fas fa-user-tie"></i> Add teacher
+                </button>
             </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <form action="{{ route('TeacherAssistant.store') }}" method="post">
+            {{ csrf_field() }}
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">User name</label><br>
+                    <input class="form-control" type="text" placeholder="Enter user name.." name="username">
+
+                </div>
+                @if ($errors->has('username'))
+                    <p class="alert alert-danger">{{ $errors->first('username') }}</p>
+                @endif
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Email</label><br>
+                    <input class="form-control" type="text" placeholder="Enter your email.." name="email">
+
+                </div>
+                @if ($errors->has('email'))
+                    <p class="alert alert-danger">{{ $errors->first('email') }}</p>
+                @endif
+                <div class="col-md-12">
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Subjects :</label><br>
+
+                    @foreach($allSubjects as $subject)
+                        <input id="{{$subject->name}}" type="checkbox" name="subjects[]" value="{{$subject->id}}"><label class="sub" for="{{$subject->name}}">{{$subject->name}}</label><br>
+                    @endforeach
+                </div>
+                @if ($errors->has('subjects'))
+                    <p class="alert alert-danger">{{ $errors->first('subjects') }}</p>
+                @endif
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Password</label><br>
+                    <input class="form-control" type="password" placeholder="Enter your password .." name="password">
+                </div>
+                @if ($errors->has('password'))
+                    <p class="alert alert-danger">{{ $errors->first('password') }}</p>
+            @endif
+            <!-- <div class="col-md-6">
+
+                        <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Date Created</label><br>
+                        <input class="form-control" type="text" placeholder="Enter Date..">
+
+                </div> -->
+            </div>
+            <div class="row my-4" style="text-align:right;">
+                <div class="col-md-3">
+                    <button class="btn adding">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
--- resources/views/admin/teacher/edit.blade.php
+++ resources/views/admin/teacher/edit.blade.php
@@ -1,28 +1,79 @@
-<form action="{{route('TeacherAssistant.update' , $teacherDetails->id)}}" method="post">
-    {{ csrf_field() }}
-    @method('PUT')
-    username:<input type="text" name="username" value="{{$teacherDetails->username}}"><br>
-    @if ($errors->has('username'))
-        <p class="alert alert-danger">{{ $errors->first('username') }}</p>
-    @endif
-    email:<input type="email" name="email" value="{{$teacherDetails->email}}">
-    <br>
-    @if ($errors->has('email'))
-        <p class="alert alert-danger">{{ $errors->first('email') }}</p>
-    @endif
-    New password<input type="text" name="password">
-    <br>
-    @if ($errors->has('password'))
-        <p class="alert alert-danger">{{ $errors->first('password') }}</p>
-    @endif
+@extends('layouts.adminapp')
+@section('content')
+    @php $noSidebar=''; @endphp
+    <div class="title3">
+        <div class="row" style="margin:0;">
+            <div class="col-md-5 my-2">
+                <button type="button" id="sidebarCollapse" class="btn stf">
+                    <i class="fas fa-user-tie"></i> Edit teacher
+                </button>
+            </div>
+        </div>
+    </div>
 
 
-    subjects<br>
-    @foreach($allSubjects as $subject)
-        <input @if(in_array($subject->id, $teacherSubjects)) {{'checked'}} @endif id="{{$subject->name}}" type="checkbox" name="subjects[]" value="{{$subject->id}}"><label for="{{$subject->name}}">{{$subject->name}}</label><br>
-    @endforeach
-    @if ($errors->has('subjects'))
-        <p class="alert alert-danger">{{ $errors->first('subjects') }}</p>
-    @endif
-    <input type="submit" name="Edit">
-</form>
+    <div class="container">
+        <form action="{{route('TeacherAssistant.update' , $teacherDetails->id)}}" method="post" class="form prof-sel">
+            {{ csrf_field() }}
+            @method('PUT')
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">User name</label><br>
+                    <input class="form-control" type="text" placeholder="Enter user name.." name="username" value="{{$teacherDetails->username}}">
+
+                </div>
+                @if ($errors->has('username'))
+                    <p class="alert alert-danger">{{ $errors->first('username') }}</p>
+                @endif
+
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Email</label><br>
+                    <input class="form-control" type="text" placeholder="Enter your email.." name="email" value="{{$teacherDetails->email}}">
+
+                </div>
+                @if ($errors->has('email'))
+                    <p class="alert alert-danger">{{ $errors->first('email') }}</p>
+                @endif
+                <div class="col-md-12">
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Subjects :</label><br>
+
+                    @foreach($allSubjects as $subject)
+                        <input @if(in_array($subject->id, $teacherSubjects)) {{'checked'}} @endif id="{{$subject->name}}" type="checkbox" name="subjects[]" value="{{$subject->id}}"><label class="sub" for="{{$subject->name}}">{{$subject->name}}</label><br>
+                    @endforeach
+                </div>
+                @if ($errors->has('subjects'))
+                    <p class="alert alert-danger">{{ $errors->first('subjects') }}</p>
+                @endif
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Password</label><br>
+                    <input class="form-control" type="password" placeholder="Enter your password .." name="password">
+                </div>
+                @if ($errors->has('password'))
+                    <p class="alert alert-danger">{{ $errors->first('password') }}</p>
+            @endif
+            <!-- <div class="col-md-6">
+
+                        <label class="mt-4 mr-2" for="inlineFormCustomSelectPref">Date Created</label><br>
+                        <input class="form-control" type="text" placeholder="Enter Date..">
+
+                </div> -->
+            </div>
+            <div class="row my-4" style="text-align:right;">
+                <div class="col-md-3">
+                    <button class="btn adding">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+@endsection
